269d3d1f6f10797b634ec65421d2a88b56947b56
JIAXInT <1131918725@qq.com>
2025-06-23 23:46:49 +0800

    Site updated: 2025-06-23 23:46:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/19/组件库打包与发布/","2025/06/21/从零构建自己的CLI工具/","2025/02/18/组件库二次封装/"];function toRandomPost(){
+var posts=["2025/06/21/从零构建自己的CLI工具/","2025/02/18/组件库二次封装/","2025/06/19/组件库打包与发布/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
